5ff21a09bd5278e4b9e457913a67299dba82d685
Lauri Tirkkonen <lauri@hacktheplanet.fi>
2022-06-08 21:56:02 +0900

    use jpdb word search instead of text parser

--- jpdb.py
+++ jpdb.py
@@ -5,35 +5,44 @@ import urllib.parse
 import os
 import sys
 import typing
+import logging
 
-def jpdb_add_text(text: str) -> None:
+import bs4
+
+logging.basicConfig(format='{asctime} {levelname} {message}',
+        style='{', level=logging.INFO)
+log = logging.getLogger()
+
+def jpdb_add_word(word: str) -> None:
     conn = http.client.HTTPSConnection('jpdb.io')
     sid = os.environ['JPDB_SID']
     deck = 1
     hdrs = {'content-type': 'application/x-www-form-urlencoded',
             'cookie': f'sid={sid}'}
-    params = urllib.parse.urlencode({'id': deck, 'text': text})
-    conn.request('POST', '/add_to_deck_from_text', body=params, headers=hdrs)
+    params = urllib.parse.urlencode({'q': word, 'lang': 'japanese'})
+    srch = f'/search?{params}'
+    conn.request('GET', f'/search?{params}', headers=hdrs)
     resp = conn.getresponse()
-    print(f'add {text}:', resp.status, resp.reason)
-    while chunk := resp.read(4096):
-        print(repr(chunk))
-    if resp.status != 302:
-        raise ValueError('not redirected from add_to_deck_from_text')
-    loc = resp.getheader('Location')
-    print(f'redir: {loc}')
-    #conn.request('GET', loc, headers=hdrs)
-    #resp = conn.getresponse()
-    #print(resp.status, resp.reason)
-    #while chunk := resp.read(4096):
-    #    print(repr(chunk))
-    confirm_id = urllib.parse.parse_qs(urllib.parse.urlparse(loc).query)['id'][0]
-    params = urllib.parse.urlencode({'id': confirm_id})
-    conn.request('POST', '/add_to_deck_from_text_confirm', body=params, headers=hdrs)
+    if resp.status != 200:
+        raise ValueError(f'got resp {resp.status} from jpdb.io/search')
+    soup = bs4.BeautifulSoup(resp, 'html.parser')
+    res = soup.find(id='result-0')
+    resword = ''.join(x for x in res.ruby if not isinstance(x, bs4.element.Tag))
+    if word != resword:
+        log.warning(f'{word!r} replaced by {resword!r}')
+        word = resword
+    
+    form = res.find('form', action='/deck/1/add')
+    if not form:
+        log.warning(f'form not found; {word} already in deck?')
+        return
+    inputs = form.find_all('input', type='hidden')
+    params = urllib.parse.urlencode({i.attrs['name']: i.attrs['value'] for i in inputs})
+    conn.request('POST', '/deck/1/add', body=params, headers=hdrs)
     resp = conn.getresponse()
-    print(f'confirm {text}:', resp.status, resp.reason)
+    log.info(f'added {word} {resp.status} {resp.reason}')
     while chunk := resp.read(4096):
         pass
 
 if __name__ == '__main__':
-    jpdb_add_text(sys.argv[1])
+    jpdb_add_word(sys.argv[1])
